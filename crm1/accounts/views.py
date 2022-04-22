63a828d0d8473f5bd8585d1aadd62826d2fa2217
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-04-23 00:21:13 +0700

    video 16: User Profile With One To One Relationship

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -28,6 +28,9 @@ def registerPage(request):
 
             group = Group.objects.get(name="customer")
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
 
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
@@ -75,8 +78,20 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    
+    print('ORDERS:', orders)
+    
+    context = {'orders':orders, 'total_orders':total_orders,
+                'delivered':delivered, 'pending':pending}
     return render(request, 'accounts/user.html', context)
 
 
