51b4ed6c3538e6e1672dd768e0817a62f9f5f7e0
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-04-23 20:45:11 +0700

    video 20: Password Reset Email

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -135,4 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'laikhanhtoanb10@gmail.com'
+EMAIL_HOST_PASSWORD = 'your email password'
