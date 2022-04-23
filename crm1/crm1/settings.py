6476781707520b17680c976948f5f78712933e13
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-04-23 12:52:23 +0700

    video 17: Image File Upload to User Profile Model

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -113,6 +113,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -126,6 +128,8 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
