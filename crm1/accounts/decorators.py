905b67eb5853b24aa1007340c7336ce08484a735
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-04-23 18:01:39 +0700

    video 19: Creating Customer Profiles with Django Signals

--- crm1/accounts/decorators.py
+++ crm1/accounts/decorators.py
@@ -1,32 +1,30 @@
-from django.http import HttpResponse
+import django
 from django.shortcuts import redirect
+from django.http import HttpResponse
+
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home')
-        else:
-            return view_func(request, *args, **kwargs)
-    
+        
+        return view_func(request, *args, **kwargs)  
     return wrapper_func
 
 def allowed_users(allowed_roles=[]):
-    def decorator(view_fun):
-        def wrapper_func(request, *args, **kwargs):
-
+    def decorator(view_func):
+        def wrapper_func(request,*args,**kwargs):
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-            
             if group in allowed_roles:
-                return view_fun(request, *args, **kwargs)
-            else:
-                return HttpResponse('You are not authorized to view this page')
+                return view_func(request,*args,**kwargs)
+            return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
 
 def admin_only(view_func):
-    def wrapper_func(request, *args, **kwargs):
+    def wrapper_function(request, *args, **kwargs):
         group = None
         if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
@@ -37,4 +35,4 @@ def admin_only(view_func):
         if group == 'admin':
             return view_func(request, *args, **kwargs)
     
-    return wrapper_func
+    return wrapper_function
