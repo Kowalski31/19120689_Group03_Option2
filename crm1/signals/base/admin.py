f2cb38684437e925a54ececc9b230964f66bc43c
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-04-24 15:30:33 +0700

    video 18: Introduction to Django Signals

---
+++ crm1/signals/base/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from base.models import Profile
+
+# Register your models here.
+
+admin.site.register(Profile)
---
+++ crm1/signals/base/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank= True,null= True)
+    first_name = models.CharField(max_length=200, null = True, blank= True)
+    last_name = models.CharField(max_length=200, null = True, blank= True)
+    Phone = models.CharField(max_length=200, null = True, blank= True)
+
+    def __str__(self) -> str:
+        return str(self.user)
